afbe320b73b921f05bee68d48c335b15692c24f5
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-17 18:08:45 -0400

    Start + Cancel POMO

--- src/PomodoroTimer.svelte
+++ src/PomodoroTimer.svelte
@@ -3,6 +3,10 @@
   const secondsToMinutes = seconds => Math.floor(seconds / 60);
   const padWithZeroes = number => number.toString().padStart(2, "0");
 
+  const LONG_BREAK_S = minutesToSeconds(20);
+  const SHORT_BREAK_S = minutesToSeconds(5);
+  let completedPomodoros = 0;
+
   // length of the pomodoro in seconds
   const POMODORO_S = minutesToSeconds(25);
 
@@ -15,14 +19,53 @@
     return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
   }
 
+  let interval;
   function startPomo() {
-    setInterval(() => {
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        completePomodoro();
+      }
+
       pomodoroTime -= 1;
     }, 1000);
+  }
+
+  function completePomodoro() {
+    clearInterval(interval);
+    completedPomodoros++;
+
+    completedPomodoros++;
+    if (completedPomodoros === 4) {
+      rest(LONG_BREAK_S);
+    } else {
+      rest(SHORT_BREAK_S);
+    }
+  }
+
+  function rest(time) {
+    pomodoroTime = time;
+    interval = setInterval(() => {
+      if (pomodoroTime === 0) {
+        idle();
+      }
+      pomodoroTime -= 1;
+    }, 1000);
+  }
+
+  function idle() {
+    clearInterval(interval);
+    pomodoroTime = POMODORO_S;
+  }
+
+  function cancelPomo() {
+    // TODO: Add some logic to prompt user to write down
+    //   cause of interruption
+    idle();
   }
 </script>
 
 <section>
   <p>{formatTime(pomodoroTime)}</p>
   <button on:click={startPomo}>start</button>
+  <button on:click={cancelPomo}>cancel</button>
 </section>
