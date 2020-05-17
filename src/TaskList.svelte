5b060134759f542623553fc447cece87880ef606
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-16 23:17:03 -0400

    Add removeTask button

--- src/TaskList.svelte
+++ src/TaskList.svelte
@@ -7,6 +7,17 @@
     new Task("Riverbeast"),
     new Task("Manchvegas")
   ];
+
+  function addTask() {
+    //   tasks.push won't work because svelte works with change in
+    //   assignment
+    tasks = tasks.concat(new Task());
+  }
+
+  function removeTask(task) {
+    const index = tasks.indexOf(task);
+    tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+  }
 </script>
 
 <style>
@@ -30,5 +41,7 @@
         type="number"
         bind:value={task.expectedPomodoros} />
     </li>
+    <button on:click={() => removeTask(task)}>X</button>
   {/each}
 </ul>
+<button on:click={addTask}>Add a new task</button>
