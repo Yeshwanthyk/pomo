ae0efcc237aceae681d14bd360594e3ecb6e567b
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-16 23:01:19 -0400

    Get two way binding on tasks

--- src/TaskList.svelte
+++ src/TaskList.svelte
@@ -1,9 +1,34 @@
 <script>
-  const tasks = ["One world", "One ring", "Slingshot cops"];
+  import { afterUpdate } from "svelte";
+  import { Task } from "./Task.js";
+
+  let tasks = [
+    new Task("Slingshot"),
+    new Task("Riverbeast"),
+    new Task("Manchvegas")
+  ];
 </script>
+
+<style>
+  ul {
+    list-style: none;
+  }
+  .description {
+    min-width: 400px;
+  }
+  .pomodoro {
+    max-width: 100px;
+  }
+</style>
 
 <ul>
   {#each tasks as task}
-    <li>{task}</li>
+    <li>
+      <input class="description" type="text" bind:value={task.description} />
+      <input
+        class="pomodoro"
+        type="number"
+        bind:value={task.expectedPomodoros} />
+    </li>
   {/each}
 </ul>
